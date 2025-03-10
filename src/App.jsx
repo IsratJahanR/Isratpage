1a5cdb1109478562b3173273dcb8fe642512a996
IsratJahanR <isratjahan151q@gmail.com>
2025-03-11 00:03:36 +0600

    install tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 
-import styles from './App.module.css';
+import styles from "./App.module.css";
+// import { About } from "./components/About/About";
+// import { Contact } from "./components/Contact/Contact";
+// import { Experience } from "./components/Experience/Experience";
+// import { Hero } from "./components/Hero/Hero";
+// import { Navbar } from "./components/Navbar/navbar";
+//import { Projects } from "./components/Projects/Projects";
 
 function App() {
 
-  return <div className="styles.App">Hello World</div>;
+  return <div className="styles.App">
+    <navbar />
+      {/* <Hero />
+      <About />
+      <Experience />
+      <Projects />
+      <Contact /> */}
+  </div>;
   
 }
 
